bc5911481e6e2e9599a3b0e6c9f001d742077461
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2018-11-26 16:31:56 -0500

    Conducted two-way ANOVA; calculated effect size

--- src/Rscript_maglio.R
+++ src/Rscript_maglio.R
@@ -2,6 +2,8 @@
 
 # Load dependencies
 library(tidyverse)
+library(car)
+library(broom)
 
 # Set working directory
 setwd("~/GitHub/osl-maglio-polman-2014/src")
@@ -16,16 +18,35 @@ maglio %>%
   group_by(direction, station) %>%
   summarize(mean = mean(subjective_distance), count = n())
 
-# Station and direction into factor
+# Station, direction, orientation into factor
 maglio_clean <- within(maglio, {
   station <- factor(station, labels = c("spadina", "st_george", "bloor_yonge", "sherbourne"))
   direction <- factor(direction)
+  orientation <- factor(orientation, labels = c("towards", "away"))
 })
 
 ####### TWO-WAY ANOVA #######
+
+# Construct aov object
+maglio_aov <- aov(subjective_distance ~ orientation * station, data = maglio_clean)
+
+# Conduct ANOVA with type III sum of squares
+options(contrasts = c("contr.sum", "contr.poly"))
+(maglio_anova <- Anova(maglio_aov, type = "III"))
+
+# Tidy model to calculate effect size
+(maglio_tidy <- tidy(maglio_anova))
+
+# Effect size for station
+(eta_station <- maglio_tidy$sumsq[3] / (maglio_tidy$sumsq[3] + maglio_tidy$sumsq[5]))
+
+# Effect size for interaction
+(eta_interact <- maglio_tidy$sumsq[4] / (maglio_tidy$sumsq[4] + maglio_tidy$sumsq[5]))
 
 # Outliers?
 
 # Normality?
 
 # Homoscedasticity?
+plot(maglio_aov, 1)
+
