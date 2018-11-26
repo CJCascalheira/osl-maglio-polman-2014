86c0524cd1d7c462e8a25a483d09391a340ba26b
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2018-11-26 09:00:04 -0500

    Established directory structure

---
+++ src/Rscript_maglio.R
@@ -0,0 +1,13 @@
+####### SETUP WORKSPACE #######
+
+# Load dependencies
+library(tidyverse)
+
+# Set working directory
+setwd("~/GitHub/osl-maglio-polman-2014/src")
+
+# Import data
+maglio <- read_csv("../data/Maglio and Polman 2014 Experiment 1.csv")
+
+####### CLEAN DATA #######
+
