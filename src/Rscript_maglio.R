889f0bf0627813f06b63ead8f00ae1895393a959
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2018-11-26 12:13:31 -0500

    Introduced factorial ANOVA

--- src/Rscript_maglio.R
+++ src/Rscript_maglio.R
@@ -11,3 +11,21 @@ maglio <- read_csv("../data/Maglio and Polman 2014 Experiment 1.csv")
 
 ####### CLEAN DATA #######
 
+# Which stations correpond to the integers
+maglio %>%
+  group_by(direction, station) %>%
+  summarize(mean = mean(subjective_distance), count = n())
+
+# Station and direction into factor
+maglio_clean <- within(maglio, {
+  station <- factor(station, labels = c("spadina", "st_george", "bloor_yonge", "sherbourne"))
+  direction <- factor(direction)
+})
+
+####### TWO-WAY ANOVA #######
+
+# Outliers?
+
+# Normality?
+
+# Homoscedasticity?
